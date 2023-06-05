4f102159a6b93afc87968b7b25ca0c0ecf8e648a
Macsoftechnologies <macsoftechnologies@gmail.com>
2023-06-05 11:38:24 +0530

    Project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
-
+import data from './data.json';
+import { useState } from 'react';
+import { Input } from "antd";
+import { StarFilled } from '@ant-design/icons';
 function App() {
+ 
+const [fetchEl, setFetchEl] = useState([])
+const { Search } = Input;
+const onSearch = (value) => {
+  console.log(value,"valueeeee")
+  if(value !== ''){
+        const filterData = data.filter((item)=>item.category.toLowerCase().includes(value.toLowerCase()))
+         //item.category.toLowerCase().includes(value.toLowerCase())||item.name.toLowerCase().includes(value.toLowerCase()))
+        console.log(filterData);
+         setFetchEl(filterData);
+      }
+      else{
+        setFetchEl([])
+      }
+};
+// const getInputHandler =(e)=>{
+//  console.log(e.target.value) 
+
+//  setInputEl(e.target.value)
+//   console.log('Before if')
+//   
+// }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App body">
+      <div>
+        <h1>Test Project</h1>
+     {/* <input className='input' type='text' value={inputEl} onChange={getInputHandler}/> */}
+     <Search
+        size="medium"
+        placeholder="Type to search"
+        allowClear
+        style={{ width: 800, paddingBottom: 30, paddingTop: 5 }}
+        onSearch={onSearch}
+       
+        
+      />
+    <div className='container'>
+      <div className='row rowStyle'>
+        
+    <div className='col-md-12'><ul className='ul'>{fetchEl.map((el)=>{
+      return(<li className='li' key={el.id}>
+        <img src={el.image}/>
+        <div className='detailContainer'>
+        <h4>{el.name}</h4>
+        <p className='fontSpace'>{el.contactInfo}</p>
+        <p>{el.address}</p>
+        <p>{el.rating}<StarFilled style={{color:'#ffb000'}} /></p>
+        </div>
+        
+      </li>)
+     })}</ul>
+     </div>
+     </div>
+     </div> 
+    </div>
     </div>
   );
 }
